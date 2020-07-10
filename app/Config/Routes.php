1ddd0d26902e8c8d4415f5f4b24598826cc2bc36
Abilio Posada <abilioposada@gmail.com>
2020-07-09 19:37:48 -0600

    Cambia controlador y acción por defecto

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,8 +16,8 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
-$routes->setDefaultMethod('index');
+$routes->setDefaultController('Inicio');
+$routes->setDefaultMethod('inicio');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
 $routes->setAutoRoute(true);
@@ -30,7 +30,7 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Inicio::inicio');
 
 /**
  * --------------------------------------------------------------------
