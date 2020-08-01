ce20ba2412d9543a79ca5e782cd29835f6aa42c7
Abilio Posada <abilioposada@gmail.com>
2020-08-01 04:41:23 -0600

    Agrega localización personalizada al sistema

---
+++ app/Language/en/App.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+	'general'	=> [
+		'titulo'	=> 'Bookshop Application',
+		'derechos'	=> 'All rights reserved',
+		'listado'	=> '{0, plural, =0{Nothing to list} other{List}}',
+
+		'accion'	=> [
+			'nombre'	=> 'Action',
+			'guardar'	=> 'Save',
+			'agregar'	=> 'Add',
+			'agregando'	=> 'Adding',
+			'editar'	=> 'Edit',
+			'editando'	=> 'Editing',
+			'eliminar'	=> 'Delete',
+			'retornar'	=> 'Return',
+			'confirmar'	=> 'Confirm',
+			'exitosa'	=> 'Action completed successfully'
+		]
+	],
+
+	'autor'	=> [
+		'nombre'		=> 'Name',
+		'nacionalidad'	=> 'Nationality',
+		'titulo'		=> '{0, plural, =0{No authors} =1{Author} other{Authors}}',
+
+		'genero'		=> [
+			'nombre'	=> 'Genre',
+			'masculino'	=> 'Male',
+			'femenino'	=> 'Female',
+			'otro'		=> 'Other'
+		]
+	],
+
+	'libro'	=> [
+		'isbn'		=> 'ISBN',
+		'titulo'	=> 'Title',
+		'nombre'	=> '{0, plural, =0{No books} =1{Book} other{Books}}'
+	]
+];
---
+++ app/Language/es/App.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+	'general'	=> [
+		'titulo'	=> 'Aplicación Librería',
+		'derechos'	=> 'Todos los derechos reservados',
+		'listado'	=> '{0, plural, =0{Nada que listar} other{Listado}}',
+		'accion'	=> [
+			'nombre'	=> 'Acción',
+			'guardar'	=> 'Guardar',
+			'agregar'	=> 'Agregar',
+			'agregando'	=> 'Agregando',
+			'editar'	=> 'Editar',
+			'editando'	=> 'Editando',
+			'eliminar'	=> 'Eliminar',
+			'retornar'	=> 'Retornar',
+			'confirmar'	=> 'Confirmar',
+			'exitosa'	=> 'Acción realizada exitosamente'
+		]
+	],
+
+	'autor'	=> [
+		'nombre'		=> 'Nombre',
+		'nacionalidad'	=> 'Nacionalidad',
+		'titulo'		=> '{0, plural, =0{Sin autores} =1{Autor} other{Autores}}',
+		'genero'		=> [
+			'nombre'	=> 'Género',
+			'masculino'	=> 'Masculino',
+			'femenino'	=> 'Femenino',
+			'otro'		=> 'Otro'
+		]
+	],
+
+	'libro'	=> [
+		'isbn'		=> 'ISBN',
+		'titulo'	=> 'Título',
+		'nombre'	=> '{0, plural, =0{Sin libros} =1{Libro} other{Libros}}',
+	]
+];
