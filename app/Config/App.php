8b10528908fbd2c1ef25d44f2ae49f932e48d491
Abilio Posada <abilioposada@gmail.com>
2020-08-01 04:48:13 -0600

    Cambia configuración
    Cambia configuración localización
    Agrega constantes útiles

--- app/Config/App.php
+++ app/Config/App.php
@@ -63,7 +63,7 @@ class App extends BaseConfig
 	| should run under for this request.
 	|
 	*/
-	public $defaultLocale = 'en';
+	public $defaultLocale = 'es';
 
 	/*
 	|--------------------------------------------------------------------------
@@ -76,7 +76,7 @@ class App extends BaseConfig
 	| If false, no automatic detection will be performed.
 	|
 	*/
-	public $negotiateLocale = false;
+	public $negotiateLocale = true;
 
 	/*
 	|--------------------------------------------------------------------------
@@ -88,7 +88,7 @@ class App extends BaseConfig
 	| found, the first locale will be used.
 	|
 	*/
-	public $supportedLocales = ['en'];
+	public $supportedLocales = [ 'en', 'es' ];
 
 	/*
 	|--------------------------------------------------------------------------
